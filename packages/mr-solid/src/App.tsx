69fc9fdad1fcfc3c115be1f0dd35514359ebf9ac
nduartech <nduartech101@gmail.com>
2024-04-23 21:58:25 -0400

    Personal Website Microfrontend Init

--- packages/mr-solid/src/App.tsx
+++ packages/mr-solid/src/App.tsx
@@ -1,14 +1,17 @@
-import type { Component } from 'solid-js';
-import './index.css'
+import { Component, splitProps } from "solid-js";
+import "./index.css";
 
-const App: Component = () => {
+const App: Component = (props: any) => {
+
+  const [dark, restProps] = splitProps(props, ["dark"]);
+
   return (
-    <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-      <h2 class="text-5xl mb-3 text-black">Sample App</h2>
-      <p class="text-black">Build on this...</p>
-      <button class="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Learn More
-      </button>
-    </div>
+      <div class="h-screen w-screen flex flex-col justify-center items-center dark:bg-black">
+        <h2 class="text-5xl text-black dark:text-white">Sample App</h2>
+        <p class="text-black dark:text-white">Build on this...</p>
+        <button class="bg-purple-900 text-white hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded">Learn More
+        </button>
+      </div>
   );
 };
 
