4103247fcc44e37051a988e2300d326babaf1e71
nduartech <nduartech101@gmail.com>
2024-05-08 00:26:13 -0400

    Tweaks

--- packages/mr-solid/src/components/nav/Nav.tsx
+++ packages/mr-solid/src/components/nav/Nav.tsx
@@ -11,18 +11,18 @@ function Nav(props: any) {
     }
   });
   return (
-    <div class="flex flex-row md:flex-col items-start justify-start w-full h-fit absolute">
+    <div class="flex flex-row md:flex-col items-start justify-start w-full h-fit absolute p-1.5">
       <div class="flex flex-col items-start justify-start w-fit h-fit navDiv mt-0.5 ml-0.5 lg:mt-2 lg:ml-2">
         <Show when={isClosed()}>
           <NavButton
-            icon='<svg class="nav w-8 w-8" width="100%" height="100%" stroke-width="0.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#ddf2ff"><path d="M3 5H21" stroke="#ddf2ff" stroke-width="0.9" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-1"></path><path d="M3 12H21" stroke="#ddf2ff" stroke-width="0.9" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-2"></path><path d="M3 19H21" stroke="#ddf2ff" stroke-width="0.9" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-3"></path></svg>'
+            icon='<svg class="nav w-6 h-6" stroke-width="0.8" fill="none" xmlns="http://www.w3.org/2000/svg" color="#ddf2ff" viewBox="2.55 4.55 18.9 14.9"><path d="M3 5H21" stroke="#ddf2ff" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-1"></path><path d="M3 12H21" stroke="#ddf2ff" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-2"></path><path d="M3 19H21" stroke="#ddf2ff" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-3"></path></svg>'
             label="Menu"
             onClick={() => {
-              document.querySelectorAll("svg.nav").forEach(svg=>svg.classList.remove("active"));
-              document.querySelectorAll("p.navIconLabel").forEach(svg=>svg.classList.remove("active"));
+              document.querySelectorAll("svg.nav").forEach(svg => svg.classList.remove("active"));
+              document.querySelectorAll("p.navIconLabel").forEach(svg => svg.classList.remove("active"));
               setTimeout(function() {
                 setIsClosed(false);
-              },300);
+              }, 300);
               let navClosed = new Event("navClosed");
               let navOpened = new Event("navOpened");
               if (!isClosed()) {
@@ -34,15 +34,17 @@ function Nav(props: any) {
             }}
           ></NavButton>
         </Show>
-        <ul class="flex flex-row lg:flex-col items-start justify-start w-fit h-fit lg:space-y-1.5 list-none">
+        <ul class="flex flex-row lg:flex-col items-center justify-center w-fit h-fit space-y-0 md:landscape:space-y-4 lg:space-y-4 space-x-5 md:landscape:space-x-0 lg:space-x-0 list-none">
           <Show when={!isClosed()}>
             <NavButton
-              icon='<svg class="nav w-8 w-8" width="100%" height="100%" stroke-width="0.9" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#ddf2ff"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#ddf2ff" stroke-width="0.9" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-4"></path></svg>'
+              icon='<svg class="nav w-4 h-4 lg:mr-1" stroke-width="0.8" fill="none" xmlns="http://www.w3.org/2000/svg" color="#ddf2ff" viewBox="6.31 6.31 11.39 11.39"><path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#ddf2ff" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round" class="svg-elem-4"></path></svg>'
               label="Close"
               onClick={() => {
-                document.querySelectorAll("svg.nav").forEach(svg=>svg.classList.remove("active"));
-                document.querySelectorAll("p.navIconLabel").forEach(svg=>svg.classList.remove("active"));
-                setTimeout(function() {setIsClosed(true);},300);
+                document.querySelectorAll("svg.nav").forEach(svg => svg.classList.remove("active"));
+                document.querySelectorAll("p.navIconLabel").forEach(svg => svg.classList.remove("active"));
+                setTimeout(function() {
+                  setIsClosed(true);
+                }, 300);
                 let navClosed = new Event("navClosed");
                 let navOpened = new Event("navOpened");
                 if (!isClosed()) {
--- packages/mr-solid/src/components/nav/NavButton.tsx
+++ packages/mr-solid/src/components/nav/NavButton.tsx
@@ -1,6 +1,6 @@
 function NavButton(props:any) {
   return (
-    <div class="navButton cursor-pointer flex flex-row space-x-1.5 items-center justify-center text-xs font-medium text-regal-blue-50 dark:text-regal-blue-50" data-path={props.path} onClick={props.onClick} innerHTML={props.icon+'<p class="navIconLabel">'+props.label+'</p>'}>
+    <div class="navButton cursor-pointer flex flex-row space-x-2 items-center justify-center text-xs font-medium text-regal-blue-50 dark:text-regal-blue-50 text-center" data-path={props.path} onClick={props.onClick} innerHTML={props.icon+'<p class="navIconLabel hidden md:flex">'+props.label+'</p>'}>
     </div>
   );
 }
