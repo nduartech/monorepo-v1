1533936cdc4c28ac69ce5f8067f7c451ce745abb
nduartech <nduartech101@gmail.com>
2024-05-09 08:36:13 -0400

    Tweaks

--- packages/mr-astro/astro.config.mjs
+++ packages/mr-astro/astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     prefetchAll: true
   },
   integrations: [
-      solid(),
+      solid({ include: "src/components/se/SE.tsx" }),
       tailwind(),
       {
           name: 'importmap-externals',
